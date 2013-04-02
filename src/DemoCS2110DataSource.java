e8d7b28a412a4a2c4cb73b35150a7803369d2562
Travis Dean <travis.j.dean@gmail.com>
2013-04-01 20:21:36 -0400

    Added jar files and demo app to test running. Demo app appears to run, still need to verify everything. More JUnit tests?

---
+++ src/DemoCS2110DataSource.java
@@ -0,0 +1,30 @@
+/**
+ * Name: Travis Dean and Alex Martinez
+ * Unix ID: tjd2qj and cam4sn
+ * Assignment: HW4
+ * Lab Section: 102
+ */
+import java.util.Scanner;
+
+
+public class DemoCS2110DataSource
+{
+    public static void main(String[] args)
+    {
+        ProfileDataSource ps = new CS2110Profile();
+        ps.init();
+
+        System.out.println("Enter an ID and Category (\"quit\" to end).");
+        System.out.print("> ");
+        Scanner scn = new Scanner(System.in);
+        String id = scn.next();
+        while ( ! id.equals("quit") ) {
+            String cat = scn.next();
+            System.out.println(id + "," + ps.getName(id) + "," + ps.getGroup(id)
+                    + "," + cat + ": " + ps.getCategoryValues(id, cat));
+            System.out.print("> ");
+            id = scn.next();
+        }
+        System.out.println("Program done.");
+    }
+}
